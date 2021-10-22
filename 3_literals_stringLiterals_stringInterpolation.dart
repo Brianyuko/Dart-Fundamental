6a8a9106e546baa02f58679031cde6730809fe9e
Brianyuko <brianyuko45@gmail.com>
2021-10-23 04:51:09 +0700

    literals, string literals, string interpolation

---
+++ 3_literals_stringLiterals_stringInterpolation.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Literals
+  int age = 17;
+  double beratBadan = 55.5;
+  String name = "Brian";
+  bool isAlive = true;
+
+  // String Literals
+  String firstName = "Brian";
+  String lastName = "Yuko";
+  String today = 'jum\'at';
+  String tommorow = "jum'at";
+
+  // String Interpolation
+  print("Nama saya $firstName $lastName");
+  print("Jumlah karakter nama saya ${firstName.length + lastName.length}");
+}
