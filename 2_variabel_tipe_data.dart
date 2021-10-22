329cf85135c544c91754c285ba46e34f67ffb179
Brianyuko <brianyuko45@gmail.com>
2021-10-23 04:50:24 +0700

    Variabel Tipe Data

---
+++ 2_variabel_tipe_data.dart
@@ -0,0 +1,30 @@
+/* 1. Number : int, double
+   2. String
+   3. Boolean
+   4. List (Array)
+   5. Set
+   6. Map
+   7. Rune
+   8. Symbol
+*/
+void main() {
+  // Int
+  int age = 17;
+  //Double
+  double beratBadan = 55.5;
+  // String
+  String name = "Brian";
+  //Null
+  String? status;
+  // Boolean
+  bool isAlive = true;
+  // Menggunakan var
+  var otomatis = "Otomatis";
+
+  print("Nama saya : $name");
+  print("Umur saya : $age");
+  print("Berat badan : $beratBadan kg");
+  print("Masih hidup? : $isAlive");
+  print("Status : $status");
+  print("Var : $otomatis");
+}
