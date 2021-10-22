98af27f307d5776c57185f42a1aed485b8c6d7d9
Brianyuko <brianyuko45@gmail.com>
2021-10-23 04:51:21 +0700

    Constant

---
+++ 4_constant.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Final -> can only be set once.
+  final firstName = "Brian";
+  final String lastName = "Yuko";
+  print("$firstName $lastName");
+
+  // Const -> Constant variables can't be assigned a value.
+  const number = 999;
+  const double multiplyNumber = number * 2.5;
+  print(number);
+  print(multiplyNumber);
+}
